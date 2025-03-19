aa1cfed55869c6dba36e3c5c629f9d2523521472
Boyowato <muhsin.abbad@gmail.com>
2025-03-19 13:49:23 +0700

    Enemy
    enemy

--- Assets/Script/Feature/Enemy/EnemyMovement.cs
+++ Assets/Script/Feature/Enemy/EnemyMovement.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+     [SerializeField]
+    private GameObject _enemyPrefab;
+     [SerializeField]
+    private float _minimumMoveTime;
+     [SerializeField]
+    private float _maximumMoveTime;
+    private float _timeUntilMove;
+    void Awake()
     {
-        
+        SetTimeUntilMove();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+           if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up  * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down  * Time.deltaTime;
+        } 
+    }
+
+    private void SetTimeUntilMove()
+    {
+        _timeUntilMove = Random.Range(_minimumMoveTime, _maximumMoveTime);
     }
 }
--- Assets/Script/Feature/Enemy/EnemySpawner.cs
+++ Assets/Script/Feature/Enemy/EnemySpawner.cs
@@ -1,8 +1,4 @@
-using Unity.Mathematics;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 public class EnemySpawner1 : MonoBehaviour
 {
@@ -13,13 +9,11 @@ public class EnemySpawner1 : MonoBehaviour
      [SerializeField]
     private float _maximumSpawnTime;
     private float _timeUntilSpawn;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         SetTimeUntilSpawn();
     }
 
-    // Update is called once per frame
     void Update()
     {
         _timeUntilSpawn -= Time.deltaTime;    
