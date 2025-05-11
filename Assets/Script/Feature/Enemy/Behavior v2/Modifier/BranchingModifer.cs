1e880c1e421249311544ff93ca6e44346c6941f0
Faiez Fattah Wisnubrata <faiezfattah2005@gmail.com>
2025-05-11 13:43:36 +0700

    wrong access modifier

--- Assets/Script/Feature/Enemy/Behavior v2/Modifier/BranchingModifer.cs
+++ Assets/Script/Feature/Enemy/Behavior v2/Modifier/BranchingModifer.cs
@@ -12,42 +12,42 @@ public class BranchingModifier : EnemyBehavior {
         _condition = condition;
     }
     
-    public virtual void OnEnter(BehaviorActor actor) {
+    public override void OnEnter(BehaviorActor actor) {
         if (_condition()) {
             _onTrue.OnEnter(actor);
         } else {
          _onFalse.OnEnter(actor);   
         }
     }
-    public virtual void OnUpdate(BehaviorActor actor) {
+    public override void OnUpdate(BehaviorActor actor) {
         if (_condition()) {
             _onTrue.OnUpdate(actor);
         } else {
          _onFalse.OnUpdate(actor);
         }
     }
-    public virtual void OnFixedUpdate(BehaviorActor actor) {
+    public override void OnFixedUpdate(BehaviorActor actor) {
         if (_condition()) {
             _onTrue.OnFixedUpdate(actor);
         } else {
          _onFalse.OnFixedUpdate(actor);
         }
     }
-    public virtual void OnExit(BehaviorActor actor) {
+    public override void OnExit(BehaviorActor actor) {
         if (_condition()) {
             _onTrue.OnExit(actor);
         } else {
          _onFalse.OnExit(actor);
         }
     }
-    public virtual void OnTriggerEnter2d(BehaviorActor actor, Collider2D collider) {
+    public override void OnTriggerEnter2d(BehaviorActor actor, Collider2D collider) {
         if (_condition()) {
             _onTrue.OnTriggerEnter2d(actor, collider);
         } else {
          _onFalse.OnTriggerEnter2d(actor, collider);   
         }
     }
-    public virtual void OnCollisionEnter2d(BehaviorActor actor, Collision2D collision) {
+    public override void OnCollisionEnter2d(BehaviorActor actor, Collision2D collision) {
         if (_condition()) {
             _onTrue.OnCollisionEnter2d(actor, collision);
         } else {
