f821175a715fc733cbba7dd5a126342af7240268
kangExcel <erkhairre@gmail.com>
2025-06-03 19:10:57 +0700

    ok pause

--- Assets/Script/Feature/UI/PauseManager.cs
+++ Assets/Script/Feature/UI/PauseManager.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
+    
     private UIDocument uiDocument;      
     private VisualElement pauseContainer;
     private bool isPaused = false;
 
     private void Awake()
     {
-                uiDocument = GetComponent<UIDocument>();
+        uiDocument = GetComponent<UIDocument>();
+    Application.runInBackground = true;
     }
 
     private void Update()
@@ -27,18 +29,22 @@ public class PauseManager : MonoBehaviour
         isPaused = true;
         Time.timeScale = 0f;
 
-            var root = uiDocument.rootVisualElement;
+        var root = uiDocument.rootVisualElement;
         pauseContainer = root.Q<VisualElement>("pausecontainer");
         pauseContainer.style.display = DisplayStyle.Flex;
 
         var btnContinue = root.Q<Button>("Continue");
-        var btnQuit     = root.Q<Button>("Quit");
+        var btnQuit = root.Q<Button>("Quit");
 
         btnContinue.clicked -= ResumeGame;
-        btnQuit.clicked     -= QuitToMainMenu;
+        btnQuit.clicked -= QuitToMainMenu;
 
-           btnContinue.clicked += ResumeGame;
-        btnQuit.clicked     += QuitToMainMenu;
+        btnContinue.clicked += ResumeGame;
+        btnQuit.clicked += QuitToMainMenu;
+        Debug.Log("Pause ON");
+Debug.Log("Continue null? " + (btnContinue == null));
+Debug.Log("Quit null? " + (btnQuit == null));
+
     }
 
     private void ResumeGame()
